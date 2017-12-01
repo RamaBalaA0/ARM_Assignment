6a66c0e600045ee5bacc1f1bb7f9f9733aacaa24
RamaBalaA0 <p.ramabala@gmail.com>
2017-12-01 22:37:59 +0530

    commented code

--- epowerx.s
+++ epowerx.s
@@ -8,29 +8,28 @@
 ;sum=sum+temp
 ;i++
 
-	 AREA     appcode, CODE, READONLY
-     EXPORT __main
-	 ENTRY 
+	AREA     appcode, CODE, READONLY
+	EXPORT __main
+	ENTRY 
 __main  FUNCTION	
 	
-
-	MOV R0,#20			;//Number of Terms which determines the accuracy
-	VMOV.F32 S1,#5		;//Exponent Power		
+	MOV R0,#20				;//Number of Terms which determines the accuracy
+	VMOV.F32 S1,#5			;//Exponent Power		
 	BL epower
-	VMOV.F32 S0,S4		;//End result of Power series
+	VMOV.F32 S0,S4			;//End result of Power series
 	
 
-epower    MOV R2,#1				;Loop Variable Register
-		  VMOV.F32 S3,#1.0		;Temporary Register
-		  VMOV.F32 S4,#1.0		;Sum Register
-L1        CMP R0,R2
-		  VMULGE.F32 S3,S3,S1	;Mulitply temp=temp*power
-		  VMOV.F32 S5,R2		;Loop Variable need not be float,so copied into float register
-		  VCVT.F32.S32 S2,S5	;Conversion from signed 32 integer to float
-		  VDIVGE.F32 S3,S3,S2	;Division temp=temp*power/i
-		  VADDGE.F32 S4,S4,S3   ;Accumulation sum=sum+temp
-		  ADDGE R2,R2,#1
-		  BGE L1
+epower     MOV R2,#1				;Loop Variable Register
+		VMOV.F32 S3,#1.0		;Temporary Register
+		VMOV.F32 S4,#1.0		;Sum Register
+L1		CMP R0,R2
+		VMULGE.F32 S3,S3,S1		;Mulitply temp=temp*power
+		VMOV.F32 S5,R2			;Loop Variable need not be float,so copied into float register
+		VCVT.F32.S32 S2,S5		;Conversion from signed 32 integer to float
+		VDIVGE.F32 S3,S3,S2		;Division temp=temp*power/i
+		VADDGE.F32 S4,S4,S3   	;Accumulation sum=sum+temp
+		ADDGE R2,R2,#1
+		BGE L1
 
 stop B stop ; stop program
      ENDFUNC
